c52e963045c2292c269ab68e27b6cd1c41ed85db
Hadnan Basilio | BomTrato <hbasilio@bomtrato.com>
2022-01-31 10:11:02 -0300

    Minor changes

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -34,8 +34,8 @@ for (i=0; i < pacientes.length; i++) {
     }
 
     if (pesoValido && alturaValida) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     } else {
         tdImc.textContent = "Altura e/ou peso inválidos!"
     }
@@ -45,39 +45,9 @@ titulo.addEventListener("click", function () {
     console.log("Olha, posso chamar uma função anônima!");
 });
 
-var botao = document.querySelector("#adicionar-paciente");
-botao.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    var form = document.querySelector("#form-adiciona");
-
-    var nome = form.nome.value;
-    var pesoTd = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    console.log(pacienteTr);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-
-});
+function calculaImc(peso, altura) {
+    var imc = 0;
+    
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+};
