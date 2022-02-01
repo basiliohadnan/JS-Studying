9e7b38ac8284d680d654c472ba1e6540c795b803
Hadnan Basilio | BomTrato <hbasilio@bomtrato.com>
2022-02-01 16:57:39 -0300

    Aula 06 check

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -3,41 +3,38 @@ titulo.textContent = "Novo título";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for (i=0; i < pacientes.length; i++) {
+for (var i=0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
 
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
+    var pesoEhValido = validaPeso(peso);
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var alturaEhValida = validaAltura(altura);
 
-    var pesoValido = true;
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    if (peso <= 0 || peso >= 999) {
+    if (!pesoEhValido) {
         console.log("Peso inválido!");
         tdImc.textContent = "Peso inválido!"
-        pesoValido = false;
+        pesoEhValido = false;
         paciente.classList.add("paciente-invalido");
     }
 
-    var alturaValida = true;
-
-    var tdAltura= paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-
-    if (altura <= 0 || altura >= 3) {
+    if (!alturaEhValida) {
         console.log("Altura inválida!");
         tdImc.textContent = "Altura inválida!"
-        alturaValida = false;
+        alturaEhValida = false;
         paciente.classList.add("paciente-invalido");
     }
 
-    if (pesoValido && alturaValida) {
+    if (pesoEhValido && alturaEhValida) {
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
-    } else {
-        tdImc.textContent = "Altura e/ou peso inválidos!"
     }
 }
 
@@ -51,3 +48,18 @@ function calculaImc(peso, altura) {
     imc = peso / (altura * altura);
     return imc.toFixed(2);
 };
+
+function validaPeso(peso) {
+    if (peso > 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function validaAltura(altura) {
+    if (altura > 0 && altura < 2.50) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- js/form.js
+++ js/form.js
@@ -2,18 +2,25 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
-
     var form = document.querySelector("#form-adiciona");
-    var paciente = obtemPacienteDoFormulario(form);
+    var paciente = getPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
+    var erros = validaPaciente(paciente);
 
+    if (erros.length > 0) {
+        exibeMensagemDeErros(erros);
+        return;
+    } 
+        var pacienteTr = montaTr(paciente);
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
     form.reset();
+
+    var mensagensDeErro = document.querySelector("#mensagens-erro");
+    mensagensDeErro.innerHTML = "";
 });
 
-function obtemPacienteDoFormulario(form) {
+function getPacienteDoFormulario(form) {
     var paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
@@ -40,8 +47,50 @@ function montaTr(paciente) {
 
 function montaTd(valor, classe) {
     var td = document.createElement("td");
-    td.textContent = valor;
     td.classList.add(classe);
+    td.textContent = valor;
 
     return td;
+}
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+function exibeMensagemDeErros(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li)
+    });
 }
